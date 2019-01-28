31660d83644b7c0fe4aa59d63f56f8940c94c7b4
xiaoshefengsheng <chenruining@qq.com>
2019-01-28 20:14:58 +0800

    add or del

---
+++ vpn-user-manager.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PASSWORD=$(tr -cd 'a-zA-z0-9' < /dev/urandom | head -c 10)
+if [ $# -ne 1 ]
+then 
+ echo "please input 'a' to add,input 'd' to del"
+ exit
+elif [ $1 == a ]
+   then    
+         read -p "add vpn-user：" USERNAME
+           if grep -w $USERNAME /etc/openvpn/psw-file
+          	then "exit"
+          	else
+             echo $USERNAME $PASSWORD >>/etc/openvpn/psw-file
+             echo -e "username: $USERNAME  password: $PASSWORD    \n吾好wiki文档: http://wiki.pipi100.com/pages/viewpage.action?pageId=7570337 或查看钉钉大群VPN文件夹 " | mail -s "WH-VPN" $USERNAME@wuhaotech.com
+             /bin/bash /etc/openvpn/transfer.sh
+           fi
+elif [ $1 == d ]
+   then
+          read -p "delete vpn-user：" USERNAME
+           if grep -w $USERNAME /etc/openvpn/psw-file
+                  then 
+             sed -i '/'"$USERNAME"'/d' /etc/openvpn/psw-file
+             /bin/bash /etc/openvpn/transfer.sh
+               else
+             echo $USERNAME not have vpn 
+           fi
+else
+ exit
+fi
